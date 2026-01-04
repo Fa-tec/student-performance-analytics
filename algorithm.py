520f183a1425fb7d49a94a4b223e6626a50776a1
Fa-tec <faiza.albalushi03@gmail.com>
2026-01-04 23:45:57 +0400

    Fix: restore function structure after loop change

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,9 @@
+# Student Performance Analytics - Weak Area Identification (FR2)
+
+def identify_weak_areas(marks, threshold=50):
+    weak_count = 0
+    status = []
+
     for i in range(len(marks)):
         # Loop change: validate mark range
         if marks[i] < 0 or marks[i] > 100:
@@ -9,3 +15,5 @@
             weak_count += 1
         else:
             status.append("Satisfactory")
+
+    return status, weak_count
